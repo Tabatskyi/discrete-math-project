49f2ee4ec656f26984fcea92fe5a22098753f9dd
Mark Tabatskyi <vasenka688@gmail.com>
2024-04-16 20:20:53 +0300

    changed threading logic

--- Program.cs
+++ Program.cs
@@ -12,95 +12,76 @@ class Program
         int[] sizes = [20, 35, 50, 75, 100, 111, 125, 150, 175, 200];
         double[] densities = [0.5, 0.6, 0.7, 0.9, 1.0];
         int experimentCount = 20;
-
         string csvHeader = "Graph Size,Density,Average Memory Used (bytes),Average Time Taken (ms)";
 
-        Task[] listTasks = new Task[densities.Length];
+        List<Task> tasks = [];
 
-        for (int j = 0; j < densities.Length; j++)
+        foreach (double density in densities)
         {
-            double density = densities[j];
-            string filePath = $"results_list_graph_{density}.csv";
+            string listFilePath = $"results_list_graph_{density}.csv";
+            string matrixFilePath = $"results_matrix_graph_{density}.csv";
 
-            listTasks[j] = Task.Run(() =>
+            tasks.Add(Task.Run(() =>
             {
-                using StreamWriter writer = new(filePath);
+                using StreamWriter writer = new(listFilePath);
                 writer.WriteLine(csvHeader);
                 foreach (int size in sizes)
                 {
-                    long totalMemoryUsed = 0;
-                    double totalTimeTaken = 0;
-
-                    for (int i = 0; i < experimentCount; i++)
-                    {
-                        var graphGenerator = new Generator(size, density);
-                        graphGenerator.GenerateGraph();
-                        var graphList = graphGenerator.graphList;
-
-                        Stopwatch stopwatch = Stopwatch.StartNew();
-                        long memoryBefore = GC.GetTotalMemory(true);
-
-                        AntColony antColony = new(size, 60, graphList, 1, 5, 0.5, 0.1);
-                        antColony.RunOptimization(10);
-
-                        long memoryAfter = GC.GetTotalMemory(false);
-                        stopwatch.Stop();
-
-                        totalMemoryUsed += memoryAfter - memoryBefore;
-                        totalTimeTaken += stopwatch.Elapsed.TotalMilliseconds;
-                    }
-                    writer.WriteLine($"{size},{density},{totalMemoryUsed / experimentCount},{totalTimeTaken / experimentCount:F3}");
+                    PerformExperiment(writer, size, density, experimentCount, true);
                     Console.WriteLine($"list: {size}, {density} done");
                 }
-            });
-        }
+            }));
 
-        Task.WaitAll(listTasks);
-
-        Task[] matrixTasks = new Task[densities.Length];
-
-        for (int j = 0; j < densities.Length; j++)
-        {
-            double density = densities[j];
-            string matrixFilePath = $"results_matrix_graph_{density}.csv";
-            
-            matrixTasks[j] = Task.Run(() =>
+            tasks.Add(Task.Run(() =>
             {
-                using StreamWriter matrixWriter = new(matrixFilePath);
-                matrixWriter.WriteLine(csvHeader);
+                using StreamWriter writer = new(matrixFilePath);
+                writer.WriteLine(csvHeader);
                 foreach (int size in sizes)
                 {
-                    long totalMemoryUsed = 0;
-                    double totalTimeTaken = 0;
-
-                    for (int i = 0; i < experimentCount; i++)
-                    {
-                        var graphGenerator = new Generator(size, density);
-                        graphGenerator.GenerateGraph();
-                        var graphMatrix = graphGenerator.graphMatrix;
-
-                        Stopwatch stopwatch = Stopwatch.StartNew();
-                        long memoryBefore = GC.GetTotalMemory(true);
-
-                        AntColony antColony = new(size, 60, graphMatrix, 1, 5, 0.5, 0.1);
-                        antColony.RunOptimization(10);
-
-                        long memoryAfter = GC.GetTotalMemory(false);
-                        stopwatch.Stop();
-
-                        totalMemoryUsed += memoryAfter - memoryBefore;
-                        totalTimeTaken += stopwatch.Elapsed.TotalMilliseconds;
-
-                        
-                    }
-                    matrixWriter.WriteLine($"{size},{density},{totalMemoryUsed / experimentCount},{totalTimeTaken / experimentCount:F3}");
+                    PerformExperiment(writer, size, density, experimentCount, false);
                     Console.WriteLine($"matrix: {size}, {density} done");
                 }
-
-            });
+            }));
         }
 
-        Task.WaitAll(matrixTasks);
+        Task.WaitAll([.. tasks]);
+    }
+
+    private static void PerformExperiment(StreamWriter writer, int size, double density, int experimentCount, bool useList)
+    {
+        long totalMemoryUsed = 0;
+        double totalTimeTaken = 0;
+
+        for (int i = 0; i < experimentCount; i++)
+        {
+            var graphGenerator = new Generator(size, density);
+            graphGenerator.GenerateGraph();
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            long memoryBefore = GC.GetTotalMemory(true);
+
+            AntColony antColony;
+            if (useList)
+            {
+                var graphList = graphGenerator.graphList;
+                antColony = new AntColony(size, 60, graphList, 1, 5, 0.5, 0.1);
+            }
+            else
+            {
+                var graphMatrix = graphGenerator.graphMatrix;
+                antColony = new AntColony(size, 60, graphMatrix, 1, 5, 0.5, 0.1);
+            }
+
+            antColony.RunOptimization(10);
+
+            long memoryAfter = GC.GetTotalMemory(false);
+            stopwatch.Stop();
+
+            totalMemoryUsed += memoryAfter - memoryBefore;
+            totalTimeTaken += stopwatch.Elapsed.TotalMilliseconds;
+        }
+
+        writer.WriteLine($"{size},{density},{totalMemoryUsed / experimentCount},{totalTimeTaken / experimentCount:F3}");
     }
 }
 
