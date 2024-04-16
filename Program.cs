3603933f324a0e40d5f0fa7ee25dbc68d6ccfdc3
Mark Tabatskyi <vasenka688@gmail.com>
2024-04-16 14:40:59 +0300

    refactoring

--- Program.cs
+++ Program.cs
@@ -1,55 +1,55 @@
 ﻿using System.Diagnostics;
 
-namespace DiscreteMathProject
+namespace DiscreteMathProject;
+
+class Program
 {
-    class Program
+    public static void Main()
     {
-        public static void Main()
+        int[] sizes = [20, 35, 50, 75, 100, 111, 125, 150, 175, 200];
+        double[] densities = [1.0];
+        int experimentCount = 20;
+
+        Task[] tasks = new Task[sizes.Length];
+
+        for (int j = 0; j < sizes.Length; j++)
         {
-            int[] sizes = [20, 35, 50, 75, 100, 111, 125, 150, 175, 200];
-            double[] densities = [1.0];
-            int experimentCount = 20;
-
-            Task[] tasks = new Task[sizes.Length];
-
-            for (int j = 0; j < sizes.Length; j++)
+            int size = sizes[j];
+            tasks[j] = Task.Run(() =>
             {
-                int size = sizes[j];
-                tasks[j] = Task.Run(() =>
+                foreach (double density in densities)
                 {
-                    foreach (double density in densities)
+                    var graphGenerator = new Generator(size, density);
+                    graphGenerator.GenerateGraph();
+                    var graphList = graphGenerator.graphList;
+
+                    long totalMemoryUsed = 0;
+                    double totalTimeTaken = 0;
+
+                    for (int i = 0; i < experimentCount; i++)
                     {
-                        var graphGenerator = new Generator(size, density);
-                        graphGenerator.GenerateGraph();
-                        var graphList = graphGenerator.graphList;
+                        Stopwatch stopwatch = Stopwatch.StartNew();
+                        long memoryBefore = GC.GetTotalMemory(true);
 
-                        long totalMemoryUsed = 0;
-                        double totalTimeTaken = 0;
+                        AntColony antColony = new AntColony(size, 60, graphList, 1, 5, 0.5, 0.1);
+                        antColony.RunOptimization(100);
 
-                        for (int i = 0; i < experimentCount; i++)
-                        {
-                            Stopwatch stopwatch = Stopwatch.StartNew();
-                            long memoryBefore = GC.GetTotalMemory(true);
+                        long memoryAfter = GC.GetTotalMemory(false);
+                        stopwatch.Stop();
 
-                            AntColony antColony = new AntColony(size, 60, graphList, 1, 5, 0.5, 0.1);
-                            antColony.RunOptimization(100);
-
-                            long memoryAfter = GC.GetTotalMemory(false);
-                            stopwatch.Stop();
-
-                            totalMemoryUsed += memoryAfter - memoryBefore;
-                            totalTimeTaken += stopwatch.Elapsed.TotalMilliseconds;
-                        }
-
-                        Console.WriteLine($"Graph size: {size}, Density: {density}");
-                        Console.WriteLine($"Average Memory Used: {totalMemoryUsed / experimentCount} bytes");
-                        Console.WriteLine($"Average Time Taken: {totalTimeTaken / experimentCount} ms");
-                        Console.WriteLine();
+                        totalMemoryUsed += memoryAfter - memoryBefore;
+                        totalTimeTaken += stopwatch.Elapsed.TotalMilliseconds;
                     }
-                });
-            }
 
-            Task.WaitAll(tasks);
+                    Console.WriteLine($"Graph size: {size}, Density: {density}");
+                    Console.WriteLine($"Average Memory Used: {totalMemoryUsed / experimentCount} bytes");
+                    Console.WriteLine($"Average Time Taken: {totalTimeTaken / experimentCount} ms");
+                    Console.WriteLine();
+                }
+            });
         }
+
+        Task.WaitAll(tasks);
     }
 }
+
